59a56b4e386eb8f22aefca28c6227772fea4a4d3
AkerKondor <kondrashkin.epm@gmail.com>
2025-01-17 20:35:51 +0300

    Update buildcache.sh

--- rpmorpan-hack/buildcache.sh
+++ rpmorpan-hack/buildcache.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 # This script requires rpmorphan package installed.
+rpmdir=$1
+rh_cache="/tmp/rpmorphan.cache"
+if [ -e $rh_cache ]
+then
+    rm -f $rh_cache
+fi    
+touch $rh_cache
+for package in `ls $rpmdir`
+do
+    rpm -q "$rpmdir""$package" --queryformat '%{NAME};[%{REQUIRENAME},] ;[%{PROVIDES},];[%{FILENAMES},];%{INSTALLTIME}\n' >> $rh_cache;
+done
